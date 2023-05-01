2e7c2ef5239c79f73c3d1eb6d5641cd8d80dea80
leesohee <dlthgml0108@daum.net>
2023-05-01 10:35:07 +0900

    feat: Codeup1103,1106

---
+++ src/main/java/lsh/codeup/Codeup1103.java
@@ -0,0 +1,7 @@
+package lsh.codeup;
+
+public class Codeup1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/lsh/codeup/Codeup1106.java
@@ -0,0 +1,9 @@
+package lsh.codeup;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        System.out.println(min + " " + max);
+    }
+}
